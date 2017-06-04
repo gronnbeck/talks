f02f68a30e52144f1dc2f804cd125dc83199bd8a
Ken Grønnbeck <kengroenn@gmail.com>
2017-06-04 15:21:44 +0200

    Make helloworldserver deployable

---
+++ high-performance-api-on-kubernetes/01-the-tools/helloworldserver/build.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -e
+
+_name="helloworldserver"
+
+#SHA=$(git rev-parse --short HEAD)
+#All builds live in production
+CONTAINER_TAG="gronnbeck/$_name"
+
+CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -a -ldflags '-s -w' -installsuffix cgo -o output/$_name main.go
+
+# Use an output folder containing only the Go binaries and the Dockerfile to minimize
+# the Docker context
+cp Dockerfile output
+docker build -t $CONTAINER_TAG .
