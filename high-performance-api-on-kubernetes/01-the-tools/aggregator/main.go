d4e0213de26dd8b7aa23e9fbcb74a7591b6f5e49
Ken Grønnbeck <kengroenn@gmail.com>
2017-06-04 16:51:59 +0200

    Add aggregator service

---
+++ high-performance-api-on-kubernetes/01-the-tools/aggregator/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+
+	vegeta "github.com/tsenart/vegeta/lib"
+)
+
+func main() {
+	serversEnv := os.Getenv("VEGETA_SERVERS")
+	runtime := os.Getenv("RUNTIME")
+
+	if serversEnv == "" && runtime != "K8S" {
+		panic("You are not running this on k8s. Need to specify servers")
+	}
+
+	servers := strings.Split(serversEnv, ",")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if runtime == "K8S" {
+			servers = vegetaserversOnKube()
+			if len(servers) == 0 {
+				log.Println("No servers found. Sleeping 60 seconds before retrying")
+				time.Sleep(60 * time.Second)
+			}
+		}
+
+		nofServers := len(servers)
+		done := make(chan vegeta.Metrics)
+
+		parts := make([]vegeta.Metrics, 0)
+
+		for i, server := range servers {
+			go func(i int, server string, parts []vegeta.Metrics) {
+				log.Printf("Fetching data from server: %v", server)
+				res, err := http.Get(server)
+				if err != nil {
+					log.Println(errors.Wrap(err, "Could not get result from server"))
+					return
+				}
+				byt, err := ioutil.ReadAll(res.Body)
+				if err != nil {
+					log.Println(errors.Wrap(err, "Could not read payload from body"))
+					return
+				}
+				var metrics vegeta.Metrics
+				err = json.Unmarshal(byt, &metrics)
+				if err != nil {
+					log.Println(errors.Wrap(err, "Could not parse data from payload"))
+					return
+				}
+
+				done <- metrics
+			}(i, server, parts)
+		}
+
+		for i := 0; i < nofServers; i++ {
+			select {
+			case m := <-done:
+				parts = append(parts, m)
+				continue
+			case <-time.After(3 * time.Second):
+				log.Println("One of the results timed-out")
+			}
+		}
+
+		byt, err := json.Marshal(parts)
+
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+
+		w.Write(byt)
+	})
+
+	http.ListenAndServe(":8082", nil)
+}
---
+++ high-performance-api-on-kubernetes/01-the-tools/aggregator/vegetaservers.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+func vegetaserversOnKube() []string {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{
+		LabelSelector: "app=vegetaserver",
+	})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var servers = make([]string, 0)
+
+	for _, pod := range pods.Items {
+		servers = append(servers, fmt.Sprintf("http://%v:8081/", pod.Status.PodIP))
+	}
+
+	return servers
+}
