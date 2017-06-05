0e86baba602aa4f88d365076450919e0e9f48a59
Ken Grønnbeck <kengroenn@gmail.com>
2017-06-05 11:59:01 +0200

    Make naive run on kube

---
+++ high-performance-api-on-kubernetes/02-the-naive/naive/build.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+set -e
+
+_name="naive"
+
+#SHA=$(git rev-parse --short HEAD)
+#All builds live in production
+CONTAINER_TAG="gronnbeck/redis-api:naive"
+
+CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -a -ldflags '-s -w' -installsuffix cgo -o output/$_name *.go
+
+# Use an output folder containing only the Go binaries and the Dockerfile to minimize
+# the Docker context
+docker build -t $CONTAINER_TAG .
---
+++ high-performance-api-on-kubernetes/02-the-naive/naive/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./naive --redis-addr=$REDIS_ADDR
